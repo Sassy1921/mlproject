c82a64133fdaa8a4afddd3220daa0824da720c5e
Soukayna mayara <soukaynamayara@gmail.com>
2024-05-25 21:25:46 +0100

    exception logging

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() #exc_info() returns a tuple of three values: the exception type, the exception value, and the traceback object
@@ -12,6 +12,5 @@ class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys): #error_message is the error message and error_detail is the sys.exc_info()
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
-
 
     
